3fafa767a6347fd097dad6a08c618d2849e7deac
kiberius <61194710+kiberius@users.noreply.github.com>
2020-04-06 21:31:17 +0300

    Update HTML code.py

--- HTML code.py
+++ HTML code.py
@@ -1,13 +1,14 @@
 # HTML code
 def main():
-
+    
+    head_text = str(input('Head text:\n'))
+    body_text = str(input('Body test:\n'))
+    
     code = '<html>\n'
     code += '<head>\n'
-    head_text = str(input('Head text:\n'))
     code += ('<h2>{}</h2>\n'.format(head_text))
     code += '</head>\n'
     code += '<body>\n'
-    body_text = str(input('Body test:\n'))
     code += ('<h4>{}</h4>\n'.format(body_text))
     code += '</body>\n'
     code += '</html>'
