76c0722a275b6779cb54ad202beffe268c3c5dd8
kiberius <61194710+kiberius@users.noreply.github.com>
2020-04-18 07:28:43 +0300

    Update Primes.py

--- Primes.py
+++ Primes.py
@@ -1,7 +1,17 @@
-for num in range(2000):
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                break
-        else:
-            print(num)
+def Main():
+    limit = int(input('\nPrime numbers limit:\n'))
+    primes_sum = 0
+    primes_count = 0
+    for num in range(limit):
+        if num > 1:
+            for i in range(2,num):
+                if (num % i) == 0:
+                    break
+            else:
+                print(num)
+                primes_sum += num
+                primes_count += 1
+    print('\nThe sum of all listed primes is {}.\n'.format(primes_sum))
+    print('The average of all listed primes is {}.\n'.format(primes_sum/primes_count))
+    
+Main()
