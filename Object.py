242204c4739a81c700fc2ad0e4f48006cedafac8
kiberius <61194710+kiberius@users.noreply.github.com>
2020-06-21 12:42:08 +0300

    Update Object.py

--- Object.py
+++ Object.py
@@ -2,12 +2,12 @@ class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         print("gettign width")
         return self._width
-    
+
     @width.setter
     def width(self, width):
         if type(width) == int or type(width) == float:
@@ -17,7 +17,7 @@ class Rectangle:
                 self._width = width
         else:
             raise ValueError("Width be a 'int' or 'float'!")
-        
+
     @property
     def height(self):
         print("getting height")
@@ -32,25 +32,25 @@ class Rectangle:
                 self._height = height
         else:
             raise ValueError("Height must be 'int' or 'float'!")
-    
+
     def area(self):
         return self.width * self.height
 
     def perimeter(self):
         return (self.width * 2) + (self.height * 2)
-    
+
     def __str__(self):
         return "Rectangle: width = {0}, height = {1}".format(self._width, self._height)
-    
+
     def __repr__(self):
         return "Rectangle({0}, {1})".format(self._width, self._height)
-    
+
     def __eq__(self, other):
         if isinstance(other, Rectangle):
             return self.width == other.width and self.height == other.height
         else:
             return NotImplemented
-        
+
     def __lt__(self, other):
         if isinstance(other, Rectangle):
             return self.area() < other.area()
